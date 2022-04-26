60203d877db9fe8c74345cb6c9ca199a808d3003
ignattru <ignatusha.evgeny@gmail.com>
2022-04-26 20:14:22 +1000

    create DAO and update class Employee

---
+++ src/main/java/com/ignatt/spring/mvc/dao/EmployeeDAOImpl.java
@@ -0,0 +1,13 @@
+package com.ignatt.spring.mvc.dao;
+
+import com.ignatt.spring.mvc.entity.Employee;
+
+import java.util.List;
+
+public class EmployeeDAOImpl implements  EmployeeDAO {
+
+    @Override
+    public List<Employee> getAllEmployee() {
+        return null;
+    }
+}
